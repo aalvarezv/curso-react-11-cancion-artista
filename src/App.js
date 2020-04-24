f5f93d79a0ebb114f265094bd0af2630d0b483e4
aalvarez <alvarez.vargasgmail.com>
2020-04-24 12:37:46 -0400

    Primer Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Fragment, useState, useEffect} from 'react'
+import Formulario from './components/Formulario'
+import Cancion from './components/Cancion'
+import Info from './components/Info'
+import axios from 'axios'
 
 function App() {
+
+  const [busquedaparams, setBusquedaParams] = useState({})
+  const [letracancion, setLetraCancion] = useState('')
+  const [informacion, setInformacion] = useState({})
+
+  useEffect(() => {
+
+    if(Object.keys(busquedaparams).length === 0){
+        return
+    }
+   
+    const consultarApiLetraArtista = async () =>{
+      const {artista, cancion} = busquedaparams
+      const url = `https://api.lyrics.ovh/v1/${artista}/${cancion}`
+      const url2 = `https://www.theaudiodb.com/api/v1/json/1/search.php?s=${artista}`
+
+      Promise.all([axios.get(url), axios.get(url2)]).then(resultado => {
+        setLetraCancion(resultado[0].data.lyrics)
+        setInformacion(resultado[1].data.artists[0])
+      })
+    }
+
+    consultarApiLetraArtista()
+
+  },[busquedaparams])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   <Fragment>
+     <Formulario 
+        setBusquedaParams = {setBusquedaParams}
+     />
+     <div className="container mt-5">
+       <div className="row">
+         <div className="col-md-6">
+            <Info 
+               informacion={informacion}
+            />
+         </div>
+         <div className="col-md-6">
+            <Cancion 
+                letra = {letracancion}
+            />
+         </div>
+       </div>
+     </div>
+   </Fragment>
+  )
 }
 
-export default App;
+export default App
